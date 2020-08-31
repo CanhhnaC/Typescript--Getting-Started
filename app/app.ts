00050eb5b16e4585d87f60c220f8cd677cee1c44
Nguyen Tran Canh <nguyentrancanh81@gmail.com>
2020-08-31 11:40:02 +0700

    Revert "Installing Typescript and Configuring a Project"
    This reverts commit dbbbd272dfcfcbeaa0c779e73bce388f8c981c35.

--- app/app.ts
+++
@@ -1,7 +0,0 @@
-function startGame() {
-  // starting a new game
-  const messagesElement = document.getElementById('messages');
-  messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';
-}
-
-document.getElementById('startGame')!.addEventListener('click', startGame);
